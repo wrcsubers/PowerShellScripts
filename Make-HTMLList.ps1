be0efff7627a9eb55cc68aab8f2b493cb81ef56e
Woods <wrcsubers@yahoo.com>
2022-03-07 13:32:14 -0800

    Added Make-HTMLList & Make-HTMLTableRow

---
+++ Make-HTMLList.ps1
@@ -0,0 +1,64 @@
+﻿#####################################################################################################
+# Make-HTMLList - https://github.com/wrcsubers/PowerShell_ScriptsAndModules
+#####################################################################################################
+
+# Purpose: This function accepts an array of strings to create an ordered or unordered HTML List
+#
+# Parameters:
+# 	- The mandatory parameter $ListItems accepts an array of strings.  Each string in the array becomes
+#	  a single list item
+#   - The optional parameter $ListTitle accepts a string.  This string becomes an h2 tag above the list.
+#   - The optional parameter $MakeUnordered is a switch.  If used, the list will an unordered (bulleted)
+#     list instead of a ordered (numbered) list.
+#
+# Usage:
+#	Input - Make-HTMLList -ListTitle 'A title for your list' -ListItems @('Item 1', 'Item 2', 'Item 3') -MakeUnordered
+# 	Output - <h2>A title for your list</h2>
+#            <ul>
+#			 	 <li>Item 1</li>
+#				 <li>Item 2</li>
+#				 <li>Item 3</li>
+#			 </ul>
+#			 <hr>
+
+
+function Make-HTMLList
+{
+    [CmdletBinding()]
+    [OutputType('String')]
+    Param
+    (
+        [Parameter(Mandatory)]
+        [String[]]$ListItems,
+        [String]$ListTitle,
+        [Switch]$MakeUnordered
+    )
+
+    Process
+    {
+        # Determine which tags to use
+        if ($MakeUnordered -eq $true){
+            $ListTags = @('<ul>','</ul>')
+        } else {
+            $ListTags = @('<ol>','</ol>')
+        }
+
+        # Title List with an h2 tag if present
+        if (($ListTitle -ne $null) -and ($ListTitle -ne '')){
+            $HTMLList += "`n<h2>$ListTitle</h2>"
+        }
+        
+        # Tag start of list
+        $HTMLList += "`n$($ListTags[0])"
+        
+        # Tag each list item
+        for ($i = 0; $i -lt $ListItems.Count; $i++){ 
+            $HTMLList += "`n`t<li>$($ListItems[$i])</li>"
+        }
+        
+        # Tag end of list
+        $HTMLList += "`n$($ListTags[1])`n<hr>"
+        
+        return $HTMLList
+    }
+}
---
+++ Make-HTMLTableRow.ps1
@@ -0,0 +1,58 @@
+﻿#####################################################################################################
+# Make-HTMLTableRow - https://github.com/wrcsubers/PowerShell_ScriptsAndModules
+#####################################################################################################
+
+# Purpose: This function accepts an array of strings and creates a single row of data for an HTML Table
+#
+# Parameters:
+# 	- The mandatory parameter $RowData accepts an array of strings.  Each string in the array becomes
+#	  a single cell value
+#   - The optional parameter $FirstColumnIsHeader is a switch.  If used, the first column in each row
+#	  will be a header.  This allows the creation of atypical tables.
+#   - The optional parameter $IsHeaderRow is a switch.  If used, this row will be a header row
+#
+# Usage:
+#	Input - Make-HTMLTableRow -RowData @('Item 1', 'Item 2', 'Item 3')
+# 	Output - <tr><td>Item 1</td><td>Item 2</td><td>Item 3</td></tr>
+
+
+function Make-HTMLTableRow
+{
+    [CmdletBinding()]
+    [OutputType('String')]
+    Param
+    (
+        [Parameter(Mandatory)]
+        [String[]]$RowData,
+        [Switch]$FirstColumnIsHeader,
+        [Switch]$IsHeaderRow
+    )
+
+    Process
+    {
+        # Tag start of row
+        $TableRowHTML = "`n`t<tr>"
+        
+        # Iterate through array and tag each element
+        for ($i = 0; $i -lt $RowData.Count; $i++){ 
+            
+            # If the first column is a header, give the first element of the row a header tag
+            if (($FirstColumnIsHeader -eq $true) -and ($i -eq 0)){
+                $TableRowHTML += "<th>$($RowData[$i])</th>"
+            } else {
+                
+                # If this is a header row, give each element of the row a header tag, otherwise give it a data tag
+                if ($IsHeaderRow -eq $true){
+                    $TableRowHTML += "<th>$($RowData[$i])</th>"
+                } else {
+                    $TableRowHTML += "<td>$($RowData[$i])</td>"
+                }
+            }
+        }
+        
+        # Tag end of row
+        $TableRowHTML += "</tr>"
+        
+        return $TableRowHTML
+    }
+}
